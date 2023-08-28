2b49e99716e36580e3336d1bd7131d0f7bfe6b43
Randy Michak <rmichak@gmail.com>
2023-08-28 16:06:33 -0400

    Update file-directory-vars.js
    added sync function calls

--- files/file-directory-vars.js
+++ files/file-directory-vars.js
@@ -1,5 +1,16 @@
 //Print the current directory using __dirname
-console.log(__dirname);
+//console.log(__dirname);
 
 //Print the current file using __filename
-console.log(__filename);
+//console.log(__filename);
+
+// Import the fs module
+//const fs = require('fs');
+
+// Create a new directory named 'newDir'
+//  fs.mkdirSync('newDir');
+//  console.log('Directory created successfully.');
+
+// List the contents of the current directory
+//  const files = fs.readdirSync('.');
+//  console.log('Directory contents:', files);
